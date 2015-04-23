6da044196a4d7c308fefa14273618159c61c0991
profh <profh@cmu.edu>
2015-04-23 13:07:22 -0400

    Added baking module back to BE helpers

--- lib/bread_express_helpers.rb
+++ lib/bread_express_helpers.rb
@@ -2,10 +2,12 @@
 require 'helpers/validations'
 require 'helpers/cart'
 require 'helpers/shipping'
+require 'helpers/baking'
 
 # create BreadExpressHelpers
 module BreadExpressHelpers
   include BreadExpressHelpers::Validations
   include BreadExpressHelpers::Shipping
   include BreadExpressHelpers::Cart
+  include BreadExpressHelpers::Baking
 end
---
+++ lib/helpers/baking.rb
@@ -0,0 +1,14 @@
+module BreadExpressHelpers
+  module Baking
+    def create_baking_list_for(category)
+      # returns a hash of item name and quantity to be baked for a 
+      # particular category of items like bread, muffins, etc.
+      all_items = Item.for_category(category).map(&:name).sort
+      baking_list = Hash[all_items.map{|name| [name, 0]}]
+      unshipped_items = OrderItem.unshipped.map{|oi| [oi.item.name, oi.quantity] if all_items.include?(oi.item.name)}.compact!
+      unshipped_items.each{|name, quant| baking_list[name] += quant} unless unshipped_items.nil?
+      baking_list.delete_if{|key, value| value == 0}
+      return baking_list
+    end
+  end  
+end
