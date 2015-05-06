84185e7759a894cb608a0a37aa2c5feceff16f9a
Israel M <purelogiq@gmail.com>
2015-05-06 03:59:59 -0400

    Top secret adjustment to errors page

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,16 +1,12 @@
 class ErrorsController < ApplicationController
-  # use minimal layout for cylon template...
-  layout "bare", only: ["cylon"]
-  
-  def cylon
+  def not_found
+    render 'internal_error'
   end
 
-  def not_found
-  end
-  
   def internal_error
+    render 'internal_error'
   end
-  
+
   def routing
     unless params[:a].nil?
       logger.info "500 Error with: '#{params[:a]}'"
